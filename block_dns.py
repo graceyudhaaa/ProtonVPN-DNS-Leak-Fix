436783dffabd772f3e60eb5ce6731276670d9189
graceyudhaaa <graceyudha@students.unnes.ac.id>
2021-09-12 21:52:29 +0700

    fix dns leak linux

--- block_dns.py
+++ block_dns.py
@@ -1,9 +1,13 @@
 import glob
 
+fix = """script-security 2
+up /etc/openvpn/update-resolv-conf
+down /etc/openvpn/update-resolv-conf"""
+
 for i in glob.glob("*.ovpn"):
     with open(i, "r") as f:
         config = f.read()
     
-    if ("block-outside-dns" not in config):
+    if (fix not in config):
         with open(i, "a") as f:
-            f.write("\nblock-outside-dns")
+            f.write(fix)
